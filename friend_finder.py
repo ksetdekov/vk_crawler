ac3b318a579c714882759d5ea0be9023db6aa693
Kirill Setdekov <ksetdekov@gmail.com>
2020-04-30 11:25:44 +0300

    print all data as output

--- friend_finder.py
+++ friend_finder.py
@@ -1,8 +1,9 @@
 from hidden import access_token
+import json
 import requests
 # 'user_id':'1631159'
 resp = requests.get(f'https://api.vk.com/method/friends.get?user_id=1631159&fields=country,bdate,sex&access_token={access_token}&v=5.103')
 jsontype = resp.json()
-# print(json.dumps(jsontype, indent=4))
+print(json.dumps(jsontype, indent=4))
 print(jsontype['response']['items'][1]['country'])
 print(jsontype['response']['count'])

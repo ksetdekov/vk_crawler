1af97800e22bdd508786097ca27d372058ed0b2e
Kirill Setdekov <ksetdekov@gmail.com>
2020-05-17 14:47:10 +0300

    get a result - done

--- search_width.py
+++ search_width.py
@@ -34,8 +34,7 @@ def search(identifier, country_to_search):
         # Only search this person if you haven't already searched them.
         if person not in searched:
             if person_is_seller(person, country_to_search):
-                print(person, " is from country ", country_to_search)
-                return True
+                return person
             else:
                 search_queue += graph[person]
                 # Marks this person as searched
@@ -43,4 +42,58 @@ def search(identifier, country_to_search):
     return False
 
 
-search(1, 9)
+end = search(1, 220)
+print(end)
+
+
+# def find_path(graph, start, end, path=[]):
+#     path = path + [start]
+#     if start == end:
+#         return path
+#     if start not in graph:
+#         return None
+#     for node in graph[start]:
+#         if node not in path:
+#             newpath = find_path(graph, node, end, path)
+#             if newpath: return newpath
+#     return None
+
+
+# find_path(graph, 1, end)
+
+def bfs_shortest_path(graph, start, goal):
+    # keep track of explored nodes
+    explored = []
+    # keep track of all the paths to be checked
+    queue = [[start]]
+
+    # return path if start is goal
+    if start == goal:
+        return "That was easy! Start = goal"
+
+    # keeps looping until all possible paths have been checked
+    while queue:
+        # pop the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        if node not in explored:
+            neighbours = graph[node]
+            # go through all neighbour nodes, construct a new path and
+            # push it into the queue
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+                # return path if neighbour is goal
+                if neighbour == goal:
+                    return new_path
+
+            # mark node as explored
+            explored.append(node)
+
+    # in case there's no path between the 2 nodes
+    return "So sorry, but a connecting path doesn't exist :("
+
+
+print(len(bfs_shortest_path(graph, 1, end)) - 1)

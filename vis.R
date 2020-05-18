851dba2ea59d9f56c42d56af5d6a3e4d340a78e1
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-05-18 10:41:38 +0300

    updated vis

--- vis.R
+++ vis.R
@@ -1,11 +1,9 @@
-
-
-data(gapminder, package = "gapminder")
-
-
 library("RSQLite")
 library(dplyr)
 library(ggplot2)
+library(openxlsx)
+library(choroplethr)
+library(choroplethrMaps)
 ## connect to db
 con <- dbConnect(drv = RSQLite::SQLite(), dbname = "clean.sqlite")
 
@@ -18,7 +16,6 @@ dbDisconnect(con)
 
 steps <- steps[steps$steps != 47, ]
 steps <- steps %>% rename(region = name, value = steps)
-library(openxlsx)
 match <- read.xlsx("match.xlsx")
 steps$region
 
@@ -26,7 +23,6 @@ for(i in seq_along(match$from)) steps$region <- gsub(match$from[i], match$to[i],
 steps <- steps[complete.cases(steps),]
 
 plotdata <- steps[,]
-library(choroplethr)
 country_choropleth(plotdata, num_colors = 9) +
         scale_fill_brewer(palette = "YlOrRd") +
         labs(
